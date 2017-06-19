e464c9f71700d03bc929b18961f9219e7e2dbefb
Leo Li <runzhou.li@gmail.com>
2017-06-19 12:33:31 -0400

    extract a helper much like node version's query handler middleware for consistency

--- api-python/api/app.py
+++ api-python/api/app.py
@@ -21,61 +21,55 @@ def index():
 
 @app.route('/events/hourly')
 def events_hourly():
-    with engine.connect() as conn:
-        result = conn.execute('''
-            SELECT date, hour, events
-            FROM public.hourly_events
-            GROUP BY date, hour
-            ORDER BY date, hour
-            LIMIT 168;
-        ''').fetchall()
-        r = [dict(row.items()) for row in result]
-        return jsonify(r)
+    return queryHelper('''
+        SELECT date, hour, SUM(events) AS events
+        FROM public.hourly_events
+        GROUP BY date, hour
+        ORDER BY date, hour
+        LIMIT 168;
+    ''')
 
 
 @app.route('/events/daily')
 def events_daily():
-    with engine.connect() as conn:
-        result = conn.execute('''
-            SELECT date, events
-            FROM public.hourly_events
-            GROUP BY date
-            ORDER BY date
-            LIMIT 7;
-        ''').fetchall()
-        r = [dict(row.items()) for row in result]
-        return jsonify(r)
+    return queryHelper('''
+        SELECT date, SUM(events) AS events
+        FROM public.hourly_events
+        GROUP BY date
+        ORDER BY date
+        LIMIT 7;
+    ''')
 
 
 @app.route('/stats/hourly')
 def stats_hourly():
-    with engine.connect() as conn:
-        result = conn.execute('''
-            SELECT date, hour,
-                SUM(impressions) AS impressions,
-                SUM(clicks) AS clicks,
-                SUM(revenue) AS revenue
-            FROM public.hourly_stats
-            GROUP BY date, hour
-            ORDER BY date, hour
-            LIMIT 168;
-        ''').fetchall()
-        r = [dict(row.items()) for row in result]
-        return jsonify(r)
+    return queryHelper('''
+        SELECT date, hour,
+            SUM(impressions) AS impressions,
+            SUM(clicks) AS clicks,
+            SUM(revenue) AS revenue
+        FROM public.hourly_stats
+        GROUP BY date, hour
+        ORDER BY date, hour
+        LIMIT 168;
+    ''')
 
 
 @app.route('/stats/daily')
 def stats_daily():
+    return queryHelper('''
+        SELECT date,
+            SUM(impressions) AS impressions,
+            SUM(clicks) AS clicks,
+            SUM(revenue) AS revenue
+        FROM public.hourly_stats
+        GROUP BY date
+        ORDER BY date
+        LIMIT 7;
+    ''')
+
+
+def queryHelper(query):
     with engine.connect() as conn:
-        result = conn.execute('''
-            SELECT date,
-                SUM(impressions) AS impressions,
-                SUM(clicks) AS clicks,
-                SUM(revenue) AS revenue
-            FROM public.hourly_stats
-            GROUP BY date
-            ORDER BY date
-            LIMIT 7;
-        ''').fetchall()
-        r = [dict(row.items()) for row in result]
-        return jsonify(r)
+        result = conn.execute(query).fetchall()
+        return jsonify([dict(row.items()) for row in result])
